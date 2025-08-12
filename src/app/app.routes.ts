6134dcac67e9af56c3035c1dcae06ea03c1edb94
Daniel Gomez <danielg200345@gmail.com>
2025-08-12 10:25:11 -0600

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Checks } from './pages/checks/checks';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'checks',
+    component: Checks
+  },
+  {
+    path: '',
+    redirectTo: 'checks',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'checks'
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Header } from './components/header/header';
+import { Checks } from './pages/checks/checks';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Checks],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+
   protected readonly title = signal('dashboard-entadas-salidas');
 }
